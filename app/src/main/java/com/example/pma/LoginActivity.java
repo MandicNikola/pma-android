a4e7bb62c105335735bd9ea796778a520d569926
marina-gavric <gavric.marinaa@gmail.com>
2020-05-30 16:23:58 +0200

    izmene

--- app/src/main/java/com/example/pma/LoginActivity.java
+++ app/src/main/java/com/example/pma/LoginActivity.java
@@ -6,15 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.pma.services.AuthPlaceholder;
+
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
     Retrofit retrofit;
+    private AuthPlaceholder service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://pma-app-19.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
     }
     public void navigateRoutes(View view){
         Intent intent = new Intent(LoginActivity.this, RouteActivity.class);
