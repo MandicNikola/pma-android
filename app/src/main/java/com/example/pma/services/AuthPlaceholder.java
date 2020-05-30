88483c6ba70cf4cb1502b4570729489a7f148823
rudananastasija <rudananastasija@uns.ac.rs>
2020-05-30 16:21:49 +0200

    registracija

--- app/src/main/java/com/example/pma/services/AuthPlaceholder.java
+++ app/src/main/java/com/example/pma/services/AuthPlaceholder.java
@@ -2,14 +2,16 @@ package com.example.pma.services;
 
 import com.example.pma.model.LoginRequest;
 import com.example.pma.model.LoginResponse;
+import com.example.pma.model.User;
+import com.example.pma.model.UserResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface AuthPlaceholder {
-   // @POST("users")
- //   Call<UserResponse> registerUser(@Body User user);
+    @POST("users")
+   Call<UserResponse> registerUser(@Body User user);
     @POST("users/login")
     Call<LoginResponse> loginUser(@Body LoginRequest user);
 
