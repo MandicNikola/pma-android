959fe3c1d877f087c7f35941f204ccaa574c696d
marina-gavric <gavric.marinaa@gmail.com>
2020-06-01 16:56:37 +0200

    Logging

--- app/src/main/java/com/example/pma/LoginActivity.java
+++ app/src/main/java/com/example/pma/LoginActivity.java
@@ -2,11 +2,17 @@ package com.example.pma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.pma.dialogues.MessageDialogue;
 
 import com.example.pma.model.LoginRequest;
 import com.example.pma.model.LoginResponse;
@@ -33,27 +39,64 @@ public class LoginActivity extends AppCompatActivity {
                 .build();
 
     }
-    public void navigateRoutes(View view){
-        service = retrofit.create(AuthPlaceholder.class);
+    public void checkData(View view){
         String username = ((EditText)findViewById(R.id.username)).getText().toString();
         String password = ((EditText)findViewById(R.id.password)).getText().toString();
+        boolean correct = true;
+            if(TextUtils.isEmpty(username)){
+                correct = false;
+                Log.d(TAG,"Username is empty");
 
-        LoginRequest request = new LoginRequest(username, password);
+            }
+            if(TextUtils.isEmpty(password)){
+                correct = false;
+                Log.d(TAG,"Password is empty");
+            }
+            if(correct){
+                LoginRequest request = new LoginRequest(username, password);
+                navigateRoutes(request);
+            }
+
+    }
+    public void navigateRoutes(LoginRequest request){
+        service = retrofit.create(AuthPlaceholder.class);
+
         Call<LoginResponse> call = service.loginUser(request);
         call.enqueue(new Callback<LoginResponse>() {
             @Override
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                 if(response.isSuccessful()){
-                    Log.d(TAG,"Successfull logged "+response.code());
-                }else{
+                    if(response.code() == 200){
+                        Log.d(TAG,"Successfull logged "+response.code());
+
+                        MessageDialogue dialog = new MessageDialogue("You have successfully logged in", "Notification");
+                        dialog.show(getSupportFragmentManager(), "logging dialog");
+                        new Handler().postDelayed(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                // your code to start second activity. Will wait for 3 seconds before calling this method
+                                Intent intent = new Intent(LoginActivity.this, RouteActivity.class);
+                                startActivity(intent);
+                            }
+                        }, 2000);
+
+                    }
+                 }else{
+                    if(response.code() == 406){
+                        CharSequence mess = "Login usuccessfull "+response.code();
+                        Context context = getApplicationContext();
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, mess, duration);
+                        toast.show();
+                    }else {
+                        Log.d(TAG, "Unsuccessfull logging" + response.code());
+                        MessageDialogue dialog = new MessageDialogue("Sign in unsuccessfully, please try again.", "Notification");
+                        dialog.show(getSupportFragmentManager(), "logging dialog");
+                    }
                     Log.d(TAG,"Unsuccessfull logged "+response.code());
                 }
-                Log.d(TAG,"Message code "+response.code());
-                if(response.body()==null){
-                    Log.d(TAG,"body is null");
-                }else{
-                    Log.d(TAG,"body is not null"+response.body().getAccessToken());
-                }
+
 
             }
 
@@ -63,8 +106,7 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-        Intent intent = new Intent(LoginActivity.this, RouteActivity.class);
-        startActivity(intent);}
+  }
 
     public void navigateRegister(View view) {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
---
+++ app/src/main/java/com/example/pma/ProfileActivity.java
@@ -0,0 +1,29 @@
+package com.example.pma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ProfileActivity extends AppCompatActivity {
+    private Spinner spinnerGender;
+    private Spinner spinnerUnits;
+    String[] gender_array = { "Male", "Female"};
+    String[] units_array = { "Metric", "Imperial"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        spinnerGender = (Spinner) findViewById(R.id.gender_spinner);
+        spinnerUnits = (Spinner) findViewById(R.id.unit_spinner);
+
+
+        ArrayAdapter adapterGender= new ArrayAdapter(this,android.R.layout.simple_spinner_item, gender_array);
+        adapterGender.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGender.setAdapter(adapterGender);
+        ArrayAdapter adapterUnit= new ArrayAdapter(this,android.R.layout.simple_spinner_item, units_array);
+        adapterUnit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerUnits.setAdapter(adapterUnit);
+    }
+}
